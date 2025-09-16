5d718e6cd47c53e13b673a39ecaf95cf0d0468ea
Álvaro Grande <alvaro.grandejimenez.ext@holcim.com>
2025-09-16 18:24:09 +0200

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
